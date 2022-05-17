d0da379a41c185f366fef7c09a4d46af329f4d05
ace34TT <tafinasoa35@gmail.com>
2022-05-17 11:27:17 +0300

    scroll

--- app/Views/frontoffice/new.php
+++ app/Views/frontoffice/new.php
@@ -15,6 +15,13 @@
 <?php $content = ob_get_clean(); ?>
 <!-- SCRIPT -->
 <?php ob_start(); ?>
-
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script>
+    jQuery(document).ready(function() {
+        jQuery('body,html').animate({
+            scrollTop: 200
+        }, 800);
+    })
+</script>
 <?php $scripts = ob_get_clean(); ?>
 <?php require('template.php'); ?>
